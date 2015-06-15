4b0eea50a1d7c5ff6b0bfa9b708f6275a39bbfee
unknown <ilantc@C403-2.iem.technion.ac.il>
2015-06-15 10:58:57 +0300

    adding bfs skeleton

--- src/VRP.py
+++ src/VRP.py
@@ -1,50 +1,59 @@
 class VRP:
     
-    def __init__(self,nTrucks,capacity,targetsData):
-        self.nTrucks = nTrucks
+    def __init__(self,nTrucks,capacity,targetsData, speed = 1):
+        self.nTrucks         = nTrucks
+        self.capacity        = capacity
+        self.speed           = speed
         self.targetLocations = [(target['x'],target['y'])       for target in targetsData]
         self.targetsWindows  = [(target['start'],target['end']) for target in targetsData]
         self.targetDurations = [target['duration']              for target in targetsData]
         self.targetDemand    = [target['demand']                for target in targetsData]
+        self.nTargets        = len(self.targetDemand)
 
-
-class solomonFileReader:
-    
-    def __init__(self):
+    def bfsConfBuilderWrapper(self, buildParam, runParam):
+        pass
+        
+    def checkFeasible(self,conf,targetId):
+        pass
+        
+    def getDistance(self,t1,t2):
         pass
     
-    def readFile(self,fileName):
-        data = {}
-        f = open(fileName)
+    def trimConfs(self,newConfs,buildParam):
+        pass
+    
+    def bfsConfBuilder(self, buildParam, runParam, lastLevelConfs):
+        newConfs = []
+        for conf in lastLevelConfs:
+            lastDistanceTravelled = self.getDistance(conf.targets[-1],0)
+            for targetId in range(self.nTargets):
+                if targetId in conf.targets:
+                    continue
+                (success, newFinishTime,newCapacity) = self.checkFeasible(conf,targetId)
+                if not success:
+                    continue
+                newConfTargets = conf.targets + [targetId]
+                newConfVal     = conf.val - lastDistanceTravelled + self.getDistance(targetId,0) 
+                
+                newConf = conf(newConfTargets, newConfVal, newFinishTime, newCapacity)
+                newConfs.append(newConf)
+        newConfs = self.trimConfs(newConfs,buildParam)
+        return newConfs
+                    
+class conf:
+    
+    def __init__(self, targets, val = -1, finishTime = -1, currentCapacity = -1):
+        self.targets = targets
+        if (val == -1):
+            (self.val,self.finishTime,self.currentCapacity) = self.calcParams(targets)
+        else: 
+            self.val = val
+            self.finishTime
+            self.currentCapacity
+    
+    # calc val as the sum of distances between the confs
+    # calc finish time as val
+    def calcParams(self, target):
+        pass
         
-        # problem name
-        data['name'] = f.next().strip()
-        # 3 blank lines
-        for _ in range(3):
-            f.next()
-        # nTrucks and capacity
-        line = f.next()
-        lineData = line.strip().split()
-        data["nTrucks"] = lineData[0]
-        data["capacity"] = lineData[1]
-        # skip 4
-        for _ in range(4):
-            f.next()
-        # targets data
-        data['targets'] = []
-        for line in f:
-            l = line.strip()
-            if l == "":
-                continue 
-            lineData            = l.split()
-            target = {}
-            target["id"]          = lineData[0]
-            target["x"]           = lineData[1]
-            target["y"]           = lineData[2]
-            target["demand"]      = lineData[3]
-            target["start"]       = lineData[4]
-            target["end"]         = lineData[5]
-            target["duration"]    = lineData[6]
-            data['targets'].append(target)
-        
-        return data
+    
